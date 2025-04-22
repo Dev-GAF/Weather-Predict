d17a320dbbd3b2bc7ee5c3024b7f97811a88c7fe
Guilherme Fortes <guilhermefortesoffice@gmail.com>
2025-04-22 19:16:36 -0300

    Almost finished
    - Each function does its part
    - Stylization
    - Docs

--- program/script.js
+++ program/script.js
@@ -1,3 +1,9 @@
+/**
+ * Makes a request to get weather data for a city.
+ * 
+ * @param {string} city - City name.
+ * @returns {Promise<Object|undefined>} Object containing API data or undefined for some error.
+ */
 async function getDataAPI(city)
 {   
     const URL = `https://purple-sun-34a4.assumpcaojeter.workers.dev/?cidade=${encodeURIComponent(city)}`;
@@ -16,9 +22,59 @@ async function getDataAPI(city)
     }
 }
 
+/**
+ * 
+ * @returns {string} - City name.
+ */
+function getCityInput() 
+{
+    return document.getElementById("city").value.trim();
+}
+
+/**
+ * Clear the weather forecast display divs.
+ */
+function clearDisplay() 
+{
+    document.getElementById("today").innerHTML = "";
+    document.getElementById("otherDays").innerHTML = "";
+}
+
+/**
+ * Makes the divs that display the weather forecast visible.
+ */
+function showElements() 
+{
+    document.getElementById("today").style.display = "block";
+    document.getElementById("otherDays").style.display = "flex";
+}
+
+/**
+ * @param {string} dataString - Date in "YYYY/MM/DD" format.
+ * @returns {string} Date formatted with weekday abbreviation and day number.
+ */
+function formatDate(dataString)
+{
+    const date = new Date(dataString + 'T00:00:00');
+    const weekDay = date.toLocaleDateString('pt-BR', { 
+        weekday: 'long'
+    });
+        
+    const nameDay = weekDay.split("-")[0].trim();
+
+    const shortenedDay = nameDay.slice(0, 3).toLowerCase();
+
+    const numberDay = date.getDate().toString().padStart(2, '0');
+
+    return `${shortenedDay}, ${numberDay}`;
+}
+
+/**
+ * Main function: fetches city data, clears the screen, displays current and future data.
+ */
 async function shape() 
 {
-    const city = document.getElementById("city").value.trim();
+    const city = getCityInput();
 
     if (!city)
     {
@@ -27,83 +83,128 @@ async function shape()
     }
 
     const data = await getDataAPI(city)
+    console.log(data);
 
-    if (!data) return;
+    if (!data || !data.forecast || !data.forecast.forecastday) {
+        alert("Invalid data returned from API.");
+        return;
+    }
+        
+    clearDisplay();
+    showElements();
 
+    renderToday(data);
+    renderOtherDays(data);
+}
+
+/**
+ * Renders the current day's weather data on the screen.
+ * 
+ * @param {Object} data - Returns data from API.
+ */
+function renderToday(data) 
+{
     const todayDiv = document.getElementById("today");
-    const otherDaysDiv = document.getElementById("otherDays");
+    const today = data.forecast.forecastday[0];
+    const currentTime = new Date(data.location.localtime);
 
-    todayDiv.innerHTML = "";
-    otherDaysDiv.innerHTML = "";
+    const cityTitle = document.createElement("h1");
+    cityTitle.textContent = data.location.name;
+    todayDiv.appendChild(cityTitle);
 
-    todayDiv.style.display = "block";
+    let closestHourData = today.hour[0];
+    let closestDiff = Math.abs(currentTime - new Date(closestHourData.time));
 
-    const forecastDays = data.forecast.forecastday;
-    
-    // ---------- TODAY ----------
-    const today = forecastDays[0];
-
-    // Name of City
-    const divCity = document.createElement("div");
-
-    const titleCity = document.createElement("h1");
-    titleCity.textContent = data.location.name;
-
-    divCity.appendChild(titleCity)
-
-    // IMG and Temperature
-    const currentHour = new Date(data.location.localtime).getHours();
-
-    const hourDataNow = today.hour.find(h => {
-        const hour = new Date(h.time).getHours();
-        return hour === currentHour;
-    });
-
-    const currentForecast = hourDataNow || today.hour[0]
+    for (let i = 1; i<today.hour.length; i++) 
+    {
+        const hourTime = new Date(today.hour[i].time);
+        const diff = Math.abs(currentTime - hourTime);
+        if (diff < closestDiff) 
+        {
+            closestDiff = diff;
+            closestHourData = today.hour[i];
+        }
+    }
 
     const currentInfo = document.createElement("div");
     currentInfo.id = "current-info";
 
-    const img = document.createElement("img");
-    img.src = `https:${currentForecast.condition.icon}`;
-    img.alt = currentForecast.condition.text;
+    const imgToday = document.createElement("img");
+    imgToday.src = `https:${closestHourData.condition.icon}`;
+    imgToday.alt = closestHourData.condition.text;
 
     const temp = document.createElement("h1");
     temp.id = "temperature";
-    temp.textContent = `${currentForecast.temp_c}°C`;
+    temp.textContent = `${Math.round(closestHourData.temp_c)}°C`;
 
-    currentInfo.appendChild(img);
+    currentInfo.appendChild(imgToday);
     currentInfo.appendChild(temp);
 
-    const descriptionInfo = document.createElement("div");
-
     const description = document.createElement("p");
-    description.id = "description"
-    description.textContent = currentForecast.condition.text;
-
-    descriptionInfo.appendChild(description);
+    description.id = "description";
+    description.textContent = closestHourData.condition.text;
 
     const othersInfoClimate = document.createElement("div");
-    othersInfoClimate.id = "details"
+    othersInfoClimate.id = "details";
 
     const feelsLike = document.createElement("p");
-    feelsLike.textContent = `Feels Like: ${currentForecast.feelslike_c}°C`;
+    feelsLike.textContent = `Sensação térmica: ${Math.round(closestHourData.feelslike_c)}°C`;
 
     const humidity = document.createElement("p");
-    humidity.textContent = `Humidity: ${currentForecast.humidity}%`;
+    humidity.textContent = `Umidade: ${closestHourData.humidity}%`;
 
     const pressure = document.createElement("p");
-    pressure.textContent = `Pressure: ${currentForecast.pressure_mb} hPa`;
+    pressure.textContent = `Pressão: ${closestHourData.pressure_mb} hPa`;
 
-    othersInfoClimate.appendChild(feelsLike);
-    othersInfoClimate.appendChild(humidity);
-    othersInfoClimate.appendChild(pressure);
+    othersInfoClimate.append(feelsLike, humidity, pressure);
 
-    todayDiv.appendChild(divCity);
-    todayDiv.appendChild(currentInfo);
-    todayDiv.appendChild(descriptionInfo);
-    todayDiv.appendChild(othersInfoClimate);
+    todayDiv.append(currentInfo, description, othersInfoClimate);
 }
 
+/**
+ * Renders the forecast for the next 2 days.
+ * 
+ * @param {Object} data - Returns data from API.
+ */
+function renderOtherDays(data) 
+{
+    const otherDaysDiv = document.getElementById("otherDays");
+    const forecastDays = data.forecast.forecastday;
+
+    const subTitle = document.createElement("h1");
+    subTitle.textContent = "Previsão para os próximos 2 dias";
+    otherDaysDiv.appendChild(subTitle);
+
+    for (let i = 1; i<forecastDays.length; i++) 
+    {
+        const divDays = document.createElement("div");
+        divDays.id = "div-days";
+
+        const dayWeek = document.createElement("p");
+        dayWeek.textContent = formatDate(forecastDays[i].date);
+
+        const img = document.createElement("img");
+        img.src = `https:${forecastDays[i].day.condition.icon}`;
+        img.alt = forecastDays[i].day.condition.text;
+
+        const tempText = document.createElement("p");
+        tempText.textContent = `${Math.round(forecastDays[i].day.maxtemp_c)}°C / ${Math.round(forecastDays[i].day.mintemp_c)}°C`;
+
+        const desc = document.createElement("p");
+        desc.textContent = forecastDays[i].day.condition.text;
+        desc.id = "others-temp";
+
+        divDays.append(dayWeek, img, tempText, desc);
+        otherDaysDiv.appendChild(divDays);
+    }
+}
+
+/**
+ * Allows the use of the "Enter" key in the city field to search for the forecast.
+ */
+document.getElementById("city").addEventListener("keydown", function (event) {
+    if (event.key === "Enter") 
+        shape(); 
+});
 
 
